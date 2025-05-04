2dcfda7ce6c4eacd619cb91c6649863a9905f55c
ahnajoy <janoviwahyur@gmail.com>
2025-05-04 10:16:57 +0700

    Update diabetes_app.py

--- diabetes_app.py
+++ diabetes_app.py
@@ -1,20 +1,20 @@
 import streamlit as st
 
 page1 = st.Page(
-    page="views\web.py",
+    page="views/web.py",
     title="Prediksi Risiko Diabetes Melitus",
     default=True,
 )
 page2 = st.Page(
-    page="views\MassaIndex.py",
+    page="views/MassaIndex.py",
     title="Body Mass Index (BMI)",
 )
 page3 = st.Page(
-    page="views\Solusi.py",
+    page="views/Solusi.py",
     title="Diabetes Melitus",
 )
 
-st.logo("assets\Untitled (1100 x 900 px)new2.png", size='large')
+st.logo("assets/Untitled (1100 x 900 px)new2.png", size='large')
 
 pg = st.navigation(pages=[page1, page2, page3])
 
